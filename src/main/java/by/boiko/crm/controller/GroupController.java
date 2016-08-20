09a515661977b9465d7969b2a9acc0e8b88cab18
erizo_s <alexboiko1993@gmail.com>
2016-08-21 01:16:47 +0300

    test

--- src/main/java/by/boiko/crm/controller/GroupController.java
+++ src/main/java/by/boiko/crm/controller/GroupController.java
@@ -4,6 +4,7 @@ import by.boiko.crm.model.Group;
 import by.boiko.crm.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,8 +39,14 @@ public class GroupController {
 
     @RequestMapping(value = "/group/form")
     public ModelAndView openGroupForm() {
-        ModelAndView mv = new ModelAndView("form", "groupForm", new Group());
+        ModelAndView mv = new ModelAndView("formGroup", "groupForm", new Group());
         mv.addObject("groupForm");
         return mv;
+    }
+
+    @RequestMapping(value = "/group/", method = RequestMethod.POST)
+    public String saveOrUpdateStudent(@ModelAttribute("groupForm") Group group) {
+        groupService.saveOrUpdate(group);
+        return "redirect:/students";
     }
 }
